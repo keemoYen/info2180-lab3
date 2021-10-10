541dc322c5818fcad3e30e35c91bd5fe78d50894
Keemo <Keemoyen4@gmail.com>
2021-10-10 14:00:11 -0700

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,30 +2,86 @@
 window.onload = function(){
 	var body = document.getElementsByTagName("body");
 	//console.log(body);
-
-	var mydiv = document.getElementById("board");
-	//console.log(mydiv);
-	//console.log(mydiv.children);
-	let boardlist = mydiv.children;
-	//console.log(boardloc);
-
-	/*function divclassIntitialize(){
-		for(var i=0;i<boardlist.length;i++)
-			//console.log(i);
-			console.log(boardlist[i]);
-	};*/
-	boardlist[0].setAttribute("class","square");
-	boardlist[1].setAttribute("class","square");
-	boardlist[2].setAttribute("class","square");
-	boardlist[3].setAttribute("class","square");
-	boardlist[4].setAttribute("class","square");
-	boardlist[5].setAttribute("class","square");
-	boardlist[6].setAttribute("class","square");
-	boardlist[7].setAttribute("class","square");
-	boardlist[8].setAttribute("class","square");
 	
-	//console.log(boardlist[0].getAttribute("class"));
+	console.log(button);
+	var mydiv = document.getElementById("board").children;
+	console.log(mydiv);
+	//console.log(mydiv.children);
+	let boardlist = mydiv;
+	//console.log(typeof(boardlist[0]));
+	console.log(boardlist.length);
+
+
+	function divclassIntitialize(){
+		for(var i=0;i<boardlist.length;i++){
+			boardlist[i].appendChild(document.createElement("p"));
+			boardlist[i].setAttribute("class","square");
+			boardlist[i].setAttribute("id",[i]);
+			boardlist[i].addEventListener('click',clickEventHandler);
+			}
+	}
 	divclassIntitialize();
-	console.log(boardlist[0].className);
+
+
+	let firstPlayer = "X";
+	let secondPlayer = "O";
+	let flag = true;
+	let BoardState = Array.from(Array(9).keys());
+	console.log(BoardState);
+	const winCombos =[
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[6,4,2],
+	];
+	
+	function clickEventHandler(square){
+		if (flag){
+			turn(square.target.id,firstPlayer);
+			flag = false;
+			console.log(flag);
+		}
+		else{
+			turn(square.target.id,secondPlayer);
+			flag = true;
+			console.log(flag);
+		}
+		//console.log(square.target.id);
+	}
+
+	function turn(squareId,player){
+		BoardState[squareId] =player;
+		document.getElementById(squareId).innerText=player;
+	}
+
+	function newGame (){
+		console.log("print success 1");
+		for(var i=0; i<boardlist.length;i++){
+		boardlist[i].innerText='';
+		console.log("print success");
+		let BoardState = Array.from(Array(9).keys());
+		}
+	}
+	var button = document.querySelector("button");
+	button.addEventListener('click',newGame);
+
+	var divItem = document.createElement("p");
+	divItem.setAttribute("class",".square.X");
+	var divItemTextFP= document.createTextNode("X");
+	var divItemTextSP= document.createTextNode("O");
+	divItem.appendChild(divItemTextFP)
+	//console.log(boardlist[0]);
+	//console.log("mission Complete");
+	
+	//console.log(boardlist[8]);
+
+	
+	//console.log(boardlist[0].children);
+
+
 }
 
